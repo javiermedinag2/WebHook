039149f696da5f8006e55c4e3dd682e820a2e94e
Javier Medina <javiermedinag@hotmail.com>
2026-09-24 10:25:44 -0600

    Actualización 24/10/2026

--- webhook_receive.py
+++ webhook_receive.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def receive_webhook():
     data = request.get_json()
+
     print("Datos recibidos por NGROK desde Github:", data)
     # En este punto, puedes procesar los datos recibidos como desees
     return "Webhook recibido exitosamente", 200
